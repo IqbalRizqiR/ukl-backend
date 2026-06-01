a2acfba36b8fada1cf788e3948ba19341f245f1f
IqbalRizqiR <rama.ramajr121@gmail.com>
2026-06-02 06:41:45 +0700

    oke

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::createUrlUsing((function ($notifiable) {
+            $backendUrl = URL::temporarySignedRoute(
+                'verification.verify',
+                Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+                [
+                    'id' => $notifiable->getKey(),
+                    'hash' => sha1($notifiable->getEmailForVerification()),
+                ]
+            );
+            // Extract query parameters (expires, signature)
+            $parsedUrl = parse_url($backendUrl);
+            $queryString = $parsedUrl['query'] ?? '';
+
+            // Route to your frontend landing page (e.g., Next.js, Vue, Nuxt)
+            $frontendTargetUrl = config('app.frontend_url') . '/verify-email?' . $queryString;
+
+            return $frontendTargetUrl;
+        }));
     }
 }
