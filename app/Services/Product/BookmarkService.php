ee1b18d86a67b43eb099a486720240af164753f4
IqbalRizqiR <rama.ramajr121@gmail.com>
2026-06-02 18:02:13 +0700

    oke

--- app/Services/Product/BookmarkService.php
+++ app/Services/Product/BookmarkService.php
@@ -45,7 +45,6 @@ final class BookmarkService
      */
     public function getUserBookmarks(string $userId, int $perPage = 15): LengthAwarePaginator
     {
-        return Bookmark::where('user_id', $userId)
-            ->paginate($perPage);
+        return Bookmark::where('user_id', auth()->id())->get();
     }
 }
