df61e1f38202695027ec4197bb53b23b2b86077d
IqbalRizqiR <rama.ramajr121@gmail.com>
2026-06-02 17:51:32 +0700

    fix

--- app/Services/Product/BookmarkService.php
+++ app/Services/Product/BookmarkService.php
@@ -45,7 +45,7 @@ final class BookmarkService
      */
     public function getUserBookmarks(string $userId, int $perPage = 15): LengthAwarePaginator
     {
-        dd($userId);
+        dd('getUserBookmarks', Bookmark::where('user_id', $userId)->with('product')->get());
         return Bookmark::with('product')
             ->where('user_id', $userId)
             ->latest()
