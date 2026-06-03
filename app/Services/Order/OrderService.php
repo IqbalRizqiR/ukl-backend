43f7c17aa891750d566da36ce328109126968782
IqbalRizqiR <rama.ramajr121@gmail.com>
2026-06-03 07:33:08 +0700

    oke

--- app/Services/Order/OrderService.php
+++ app/Services/Order/OrderService.php
@@ -80,6 +80,9 @@ final class OrderService
                 'courier' => $data['courier'] ?? null,
                 'service' => $data['service'] ?? null,
                 'estimated_delivery_at' => $data['estimated_delivery_at'] ?? null,
+                'weight_grams' => $product->weight_grams ?? 1000,
+                'origin_city_id' => $order->seller->defaultAddress?->city_id ?? throw new \Exception('Seller has no origin city.'),
+                'destination_city_id' => $order->shippingAddress?->city_id ?? throw new \Exception('No shipping address provided.'),
             ]);
 
             EscrowTransaction::create([
